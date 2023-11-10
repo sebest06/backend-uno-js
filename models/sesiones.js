e16b8757cd57f0269659c2fb0131e0a6ea8fa4e8
sebastian oldani <sebastianoldani@gmail.com>
2023-11-10 15:36:34 -0300

    para seguir con socket

--- models/sesiones.js
+++ models/sesiones.js
@@ -7,28 +7,47 @@ class Sesion {
         this.jugadores = jugadores;
         this.jugando = jugando
         this.game = game;
+        this.players = []
     }
 }
 
 class Sesiones {
-    constructor(){
-        this.sesiones= []
+    constructor() {
+        this.sesiones = []
     }
 
-    removerSesionesBySocketId(socketId){
-               
+    removerSesionesBySocketId(socketId) {
+
         this.sesiones = this.sesiones.filter(sesion => {
             return (sesion.socketId != socketId)
         })
 
     }
 
-    addSesion(sesion = new Sesion){
-        this.sesiones.push(sesion)
+    addSesion(sesion = new Sesion) {
+        this.sesiones.push(new Sesion(sesion))
     }
 
     getSesiones() {
         return this.sesiones
+    }
+
+    getSesionById(id) {
+        let ix = this.sesiones.findIndex(p => {
+            return p.id === id
+        })
+        return ix
+    }
+
+    setJugador(gameId, nombre, password) {
+
+        console.log(this.sesiones[this.getSesionById(gameId)],"pass", this.sesiones[this.getSesionById(gameId)].password, "pass",password)
+        if (this.sesiones[this.getSesionById(gameId)].password === password) {
+            this.sesiones[this.getSesionById(gameId)].players.push(nombre)
+            this.sesiones[this.getSesionById(gameId)].jugando += 1
+            return true
+        }
+        return false
     }
 
 }
--- sockets/controller.js
+++ sockets/controller.js
@@ -3,43 +3,54 @@ const Sesiones = require("../models/sesiones");
 let sesionesID = 1;
 
 const socketController = (socket, sesiones = new Sesiones()) => {
-  //socket.on('event', data => { /* … */ });
+    //socket.on('event', data => { /* … */ });
 
-  console.log("cliente conectado");
+    console.log("cliente conectado");
 
-  socket.on("disconnect", () => {
-    console.log("cliente desconectado");
-    sesiones.removerSesionesBySocketId(socket.id)
-    socket.broadcast.emit("allSesiones", sesiones.getSesiones());
-  });
-  socket.on("salon", (payload) => {
-    sesiones.addSesion({
-      socketId: socket.id,
-      salon: payload.name,
-      password: payload.password,
-      jugadores: payload.jugadores,
-      jugando: 0,
-      id: sesionesID++,
+    socket.on("disconnect", () => {
+        console.log("cliente desconectado");
+        sesiones.removerSesionesBySocketId(socket.id)
+        socket.broadcast.emit("allSesiones", sesiones.getSesiones());
     });
-    socket.emit("allSesiones", sesiones.getSesiones());
-    socket.broadcast.emit("allSesiones", sesiones.getSesiones());
-  });
+    socket.on("salon", (payload) => {
+        sesiones.addSesion({
+            id: sesionesID++,
+            socketId: socket.id,
+            salon: payload.name,
+            password: payload.password,
+            jugadores: payload.jugadores,
+            jugando: 0,
+        });
+        socket.emit("allSesiones", sesiones.getSesiones());
+        socket.broadcast.emit("allSesiones", sesiones.getSesiones());
+    });
 
-  socket.on("getSesiones", () => {
-    socket.emit("allSesiones", sesiones.getSesiones());
-  });
+    socket.on("getSesiones", () => {
+        socket.emit("allSesiones", sesiones.getSesiones());
+    });
 
-  socket.on("enviar-mensaje", (payload, callback) => {
-    socket.broadcast.emit("enviar-mensaje", payload);
-    const id = 123456;
-    callback(id);
-  });
+    socket.on("unirse", (payload) => {
+        if (sesiones.setJugador(payload.gameId, payload.jugador, payload.password)) {
+            socket.emit("unido", { msg: "ok", gameId: payload.gameId, jugador: payload.jugador, password: payload.password})
 
-  socket.on("game", (p, id) => {
-    console.log(p, sesiones);
-  });
+            socket.emit("allSesiones", sesiones.getSesiones());
+            socket.broadcast.emit("allSesiones", sesiones.getSesiones());
+
+            console.log(sesiones.getSesiones())
+        }
+    });
+
+    /*socket.on("enviar-mensaje", (payload, callback) => {
+      socket.broadcast.emit("enviar-mensaje", payload);
+      const id = 123456;
+      callback(id);
+    })*/
+
+    socket.on("game", (p, id) => {
+        console.log(p, sesiones);
+    });
 };
 
 module.exports = {
-  socketController,
+    socketController,
 };
