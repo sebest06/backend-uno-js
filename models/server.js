28f4b80fd01b55e48f3d78ff4c1f5cfe1deda8be
sebastian oldani <sebastianoldani@gmail.com>
2023-11-23 09:27:51 -0300

    cors fixs

--- models/server.js
+++ models/server.js
@@ -11,7 +11,7 @@ class Server {
         this.server = require('http').createServer(this.app);
         this.io = require('socket.io')(this.server,{
             cors: {
-                origin: "*"
+                origin: "https://sebest06.github.io/"
               }
         });
 
